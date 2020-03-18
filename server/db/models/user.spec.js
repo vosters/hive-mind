c02f08ff8268b727e8ebc45fade9c782daae496d
Bobby Nixon <rhanixon1@gmail.com>
2020-03-18 13:29:10 -0700

    tests for all models

--- server/db/models/user.spec.js
+++ server/db/models/user.spec.js
@@ -1,34 +1,23 @@
-const {expect} = require('chai')
-const {db, Users} = require('../models')
+const { expect } = require("chai");
+const { db, User } = require(".");
 
-describe('User model', () => {
-  beforeEach(() => db.sync({force: true}))
-  describe('column definitions and validations', () => {
-    it('has a facebookId`, `email`, `password`, `username`, `photo`, `loacation`,
-    `gender`, `age`, `createdAt`, and `isAdmin` property', () => {
-      return Users.create({
-        facebookId: 'abcdefg',
-        email: 'johnsmith@gmial.com',
-        password: 'password',
-        username: 'user1',
-        photo: 'www.picture.com',
-        loacation: 12345,
-        gender: 'Male',
-        age: 21,
+describe("User model", () => {
+  beforeEach(() => db.sync({ force: true }));
+  describe("column definitions and validations", () => {
+    it("has an email, password, username, createdAt, and isAdmin property", () => {
+      return User.create({
+        email: "johnsmith@gmial.com",
+        password: "password",
+        username: "user1",
         createdAt: new Date(),
         isAdmin: false
       }).then(user => {
-        expect(user.facebookId).to.equal('abcdefg')
-        expect(user.email).to.equal('johnsmith@gmial.com')
-        expect(user.password).to.equal('password')
-        expect(payment.username).to.equal('user1')
-        expect(user.photo).to.equal('www.picture.com')
-        expect(user.loacation).to.equal(12345)
-        expect(user.gender).to.equal('Male')
-        expect(user.age).to.equal(21)
-        expect(user.createdAt).to.equal(new Date())
-        expect(user.isAdmin).to.equal(false)
-      })
-    })
-  })
-})
+        expect(user.email).to.equal("johnsmith@gmial.com");
+        expect(user.password).to.equal("password");
+        expect(user.username.to.equal("user1");
+        expect(user.createdAt).to.equal(new Date());
+        expect(user.isAdmin).to.equal(false);
+      });
+    });
+  });
+});
